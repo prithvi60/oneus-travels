bab0f2873f300c8a86290190d440d5f483669bf0
unknown <gokulgandhi97@gmail.com>
2024-06-26 10:47:23 +0530

    global card updated

--- components/GlobalTeamCard.jsx
+++ components/GlobalTeamCard.jsx
@@ -31,7 +31,7 @@ export const GlobalTeamCard = ({ title, imgSrc, desc }) => {
           </Link>
         </CardFooter>
       </Card>
-      <div className="absolute z-0 w-10 h-10 -right-2 md:w-16 md:h-16 -bottom-2 md:-bottom-5 md:-right-4 lg:right-10 xl:-right-3">
+      <div className="absolute z-0 w-10 h-10 -right-2 md:w-16 md:h-16 -bottom-2 md:-bottom-5 sm:right-4 md:-right-4 lg:right-10 xl:-right-3">
         <Image
           fill
           title="wheel svg"
@@ -46,8 +46,17 @@ export const GlobalTeamCard = ({ title, imgSrc, desc }) => {
 
 export const GlobalTeamCardReverse = ({ title, imgSrc, desc }) => {
   return (
-    <div className="relative flex flex-col-reverse w-full h-full gap-2 lg:gap-0 lg:flex-row lg:justify-end lg:items-center">
-      <Card className="w-full lg:max-w-[600px] z-20 !rounded-3xl p-2.5 md:p-5 lg:absolute lg:top-10 lg:left-16 h-auto md:h-56">
+    <div className="relative flex flex-col-reverse items-center w-full h-full space-y-2 sm:flex-row sm:justify-center md:space-y-0">
+      <div className="absolute z-10 w-10 h-10 -left-4 md:w-16 md:h-16 -bottom-6 md:-bottom-8 sm:left-4 md:-left-4 lg:left-10 xl:-left-6">
+        <Image
+          fill
+          title="wheel svg"
+          src={"/wheel.svg"}
+          alt="wheel svg"
+          className="rounded-full bg-primary"
+        />
+      </div>
+      <Card className="w-full sm:w-[50%] md:w-full lg:w-1/2 xl:w-1/2  sm:!rounded-tl-3xl sm:!rounded-bl-3xl sm:!rounded-br-none sm:!rounded-tr-none p-2.5 md:p-5 sm:!shadow-none bg-secondary text-primary h-[200px] md:h-[320px]">
         <CardHeader className="flex gap-3 font-WorkSans">
           <h5 className="text-lg font-semibold md:text-xl">{title}</h5>
         </CardHeader>
@@ -56,21 +65,22 @@ export const GlobalTeamCardReverse = ({ title, imgSrc, desc }) => {
         </CardBody>
         <CardFooter>
           <Link href={"#contact"}>
-            <PrimaryButton roleType={"button"} text={"Find out more"} />
+            <SecondaryButton roleType={"button"} text={"Find out more"} />
           </Link>
         </CardFooter>
       </Card>
-      <div className="relative w-full lg:w-3/5 h-[180px] md:h-[380px] overflow-hidden">
+      <div className="relative w-full sm:w-[40%] md:w-full lg:w-2/5 xl:w-1/2 h-[200px] md:h-[320px] overflow-hidden">
         <Image
           alt="team image"
           src={imgSrc}
+          quality={100}
           fill
           title="team image"
-          quality={100}
           sizes="(min-width: 1300px) 691px, (min-width: 1040px) calc(40vw + 179px), calc(100vw - 48px)"
-          className="object-cover object-center shadow-md rounded-3xl"
+          className="object-cover object-center shadow-md rounded-3xl sm:rounded-tr-3xl sm:rounded-br-3xl sm:rounded-bl-none sm:rounded-tl-none"
         />
       </div>
+      <div className="absolute hidden w-12 h-12 border-8 lg:w-16 lg:h-16 sm:block -z-10 top-5 -right-1 md:-right-5 lg:right-3 xl:-right-7 border-secondary rounded-xl"></div>
     </div>
   );
 };
