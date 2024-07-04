82bb45737f9d2aac8854ba3d6efe66d0a7c1184e
unknown <gokulgandhi97@gmail.com>
2024-07-04 11:31:23 +0530

    t

--- components/WelcomeSection.jsx
+++ components/WelcomeSection.jsx
@@ -4,7 +4,7 @@ import { PrimaryButton } from "./Button";
 export const WelcomeSection = () => {
   return (
     <section className="w-full h-auto px-[24px] py-7 max-w-[1200px] mx-auto space-y-4">
-      <h1 className="w-full text-[32px] font-Poppins md:text-[40px] lg:text-[42px] leading-tight text-secondary pt-5">
+      <h1 className="w-full text-[32px] font-Poppins md:text-[40px] lg:text-[42px] font-bold leading-tight text-secondary pt-5">
         Welcome to Oneus Travels. Your Global Travel Companion.
       </h1>
       <p className="text-lg font-normal font-Alice">
@@ -60,7 +60,7 @@ export const WelcomeSection = () => {
         and seamlessly supported, making us the perfect fit for your
         needs(font-Poppins).
       </p>
-      <h3 className="text-lg font-semibold font-Poppins">
+      <h3 className="text-lg font-medium font-Poppins">
         This is the OneUs Promise(font-Poppins).
       </h3>
       <div className="pt-5 w-max">
