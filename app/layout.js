1d9c1be1f6cae9cb0e93f6b42a7d6095b43d2ff8
Prithvi <prithvin19@gmail.com>
2025-04-10 17:35:53 +0530

    t

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import localFont from "next/font/local";
 import { Providers } from "./Provider";
-import GoogleAnalytics from "@/components/GoogleAnalytics";
+import GoogleAnalytics from "../libs/GoogleAnalytics";
 
 const gilroy = localFont({
   src: [
