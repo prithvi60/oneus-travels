6bc06f7ff1c4524f7fdcb18297b1d18e92c632bf
GOGA-23 <gokulgandhi97@gmail.com>
2025-04-17 16:46:00 +0530

    change

--- app/page.js
+++ app/page.js
@@ -5,29 +5,10 @@ import { GlobalTeam } from "@/components/home/GlobalTeam";
 import VideoSection from "@/components/home/VideoSection";
 import { HomePageHero } from "@/components/home/Hero";
 import DefaultLayout from "@/components/home/DefaultLayout";
-import Head from "next/head";
 
 export default function Home() {
   return (
     <main className="relative">
-      <Head>
-        {/* This appears in browser tab */}
-        <title>Tab Title - Short Version</title>
-
-        {/* These are meta titles for SEO/social */}
-        <meta
-          name="title"
-          content="Full Meta Title - Longer Descriptive Version"
-        />
-        <meta
-          property="og:title"
-          content="Social Sharing Title - Optimized Version"
-        />
-        <meta
-          name="twitter:title"
-          content="Twitter Card Title - Special Version"
-        />
-      </Head>
       <DefaultLayout>
         <HomePageHero />
         <WelcomeSection />
