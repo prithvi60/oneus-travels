e72e0a4443368e964dd41a2227d9f81773919355
GOGA-23 <gokulgandhi97@gmail.com>
2025-05-29 18:36:50 +0530

    bug fix

--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
 import { WelcomeSection } from "@/components/home/WelcomeSection";
 import { TravelTechnologyCard } from "@/components/home/TravelTechnologyCard";
-import { OurClients } from "@/components/home/OurClients";
 import { GlobalTeam } from "@/components/home/GlobalTeam";
-import VideoSection from "@/components/home/VideoSection";
 import { HomePageHero } from "@/components/home/Hero";
 import DefaultLayout from "@/components/home/DefaultLayout";
 
